bcecdf8e9aa2ed48134fd74cb93cb9c16a694b4d
Murilo Castro <murilocastro233@gmail.com>
2026-05-07 01:03:38 -0300

    ajustes finais e readme

---
+++ README.md
@@ -0,0 +1,4 @@
+Projeto Menu de camera de celular
+
+#Seleção de filtros automáticos ou personalizáveis\
+#Visualização fácil de fotos armazenadas\
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import time
-from os import remove
+
 
 # variaveis
 menu = """
@@ -16,18 +16,18 @@ menu = """
 |        finalizar        |
 +-------------------------+
 Opção: """
-foto = ""
-op = -1
-fotosSalvas = []
-filtro = "auto*"
-opcFiltro = ['auto*', 'sem filtro', 'cinza']
+foto = ""               #Adição de foto
+op = -1                 #Opção para o While Principal
+fotosSalvas = []        #Lista de fotos salvas
+filtro = "auto*"        #Filtro
+opcFiltro = ['auto*', 'sem filtro', 'cinza']   #Opções de filtro
 
 # Funções
-def tirarFotos():
+def tirarFotos():       #Cria a foto e guarda na lista
     foto = input("nome da foto: ")
     fotosSalvas.append(f'({filtro}){foto}')
 
-def msgFotos():
+def msgFotos():          #Mensagem da foto
     print("tirando foto.")
     time.sleep(0.3)
     print("tirando foto..")
@@ -35,6 +35,8 @@ def msgFotos():
     print("tirando foto...")
     time.sleep(0.3)
 
+
+#              INÍCIO
 while(op != 0):
     op = int(input(menu))
 
@@ -56,22 +58,23 @@ while(op != 0):
     elif op == 2:
         while(True):
             indf = 0
-            for f in opcFiltro: #err
+            for f in opcFiltro:     #percorre a lista de filtros para mostrar ao usuário
                 indf = indf + 1
-                print(f, indf- 1)
-            op2 = int(input(f"Selecionar Filtro (1) adicionar filtro (2) apagar filtro (3) sair (4):  "))
+                print(f'{f} id ="{indf - 1}"')
+
+            op2 = int(input("Selecionar Filtro (1) adicionar filtro (2) apagar filtro (3) sair (4):  "))  #Opções de interação
             if op2 == 1:
                 indice = int(input("Qual o número do filtro? "))
-                if indice < len(opcFiltro):
+                if indice < len(opcFiltro):    #Verifica se o índice existe
                     filtro = opcFiltro[indice]
                 else:
                     print("Número inválido")
             elif op2 == 2:
-                add = input("Digite o nome do filtro: ")
+                add = input("Digite o nome do filtro: ")   #Cria e adiciona na lista de filtros
                 opcFiltro.append(add)
             elif op2 == 3:
-                remove = int(input("Número do filtro que deseja remover: "))
-                if remove < len(opcFiltro):
+                remove = int(input("Número do filtro que deseja remover: "))    #Remove filtro pelo índice
+                if remove < len(opcFiltro):     #Verifica se o índice existe
                     opcFiltro.pop(remove)
                 else:
                     print("Número inválido")
@@ -79,24 +82,25 @@ while(op != 0):
                 break
             else:
                 print("Opção inválida")
+
     #         VER FOTOS
     elif op == 3:
         ind = 0
-        for fotos in fotosSalvas:
+        for fotos in fotosSalvas:  #percorre a lista de fotos para mostrar ao usuário
              ind = ind +1
-             print(fotos, ind- 1)
+             print(f'{fotos} id ="{ind- 1}"')
 
     #         APAGAR FOTOS
         while(True):
-            op2 = int(input("Selecionar foto para apagar (1) Apagar última foto (2) sair (3)"))
+            op2 = int(input("Selecionar foto para apagar (1) Apagar última foto (2) sair (3)")) #Opções de interação
             if op2 == 1:
-                indice = int(input("Qual o número da foto?"))
-                if indice < len(fotosSalvas):
+                indice = int(input("Qual o número da foto?"))  #Apaga foto selecionada pelo índice
+                if indice < len(fotosSalvas):   #Verifica se o índice existe
                     fotosSalvas.pop(indice)
                 else:
                     print("Número inválido")
             elif op2 == 2:
-                 fotosSalvas.pop()
+                 fotosSalvas.pop()  #Apaga última foto
             elif op2 == 3:
                 break
             else:
@@ -111,6 +115,3 @@ while(op != 0):
 
 
 
-# Adicionar lista que guarda nome das fotos e quantidade de fotos (1)
-# opção mudar filtro para auto, sem , salvos (2)
-# mostar lista de fotos com nome e quantidade (3)
